04a036538b456bff45c54307a0c68cd5d4b6494b
Wolfgang Pirker <w_pirker@gmx.de>
2022-02-07 21:44:26 +0100

    initial commit, new node project

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+const mongoUri = 'mongodb+srv://admin:testing123@cluster0.3nee9.mongodb.net/authApp?retryWrites=true&w=majority';
+mongoose.connect(mongoUri,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.set('useCreateIndex',true);
+
+/// MIDDLEWARE
+app.use(bodyParser.json());
+
+/// MODELS
+const { User } = require('./models/user');
+
+/// ROUTES
+app.post('/api/user',(req,res)=>{
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save((err,doc)=>{
+        if(err) res.status(400).send(err);
+        res.status(200).send(doc);
+    });
+})
+
+
+
+const port = process.env.PORT || 3001;
+app.listen(port,()=>{
+    console.log(`Started on port ${port}`)
+})
